2fbf3bc3b6b201d15b227af61c0da9f0e7a6ee17
Your Name <you@example.com>
2021-04-16 00:39:08 +0530

    lazy loaded angular template

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DefaultComponent } from './layout/default/default.component';
+import { FullWidthComponent } from './layout/full-width/full-width.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: DefaultComponent,
+    children: [
+      { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+      { path: 'products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) }
+    ]
+  },
+  {
+    path: '',
+    component: FullWidthComponent,
+    children: [
+      { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) }
+    ]
+  }
+  ,
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule],
+  providers: []
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { DefaultComponent } from './layout/default/default.component';
+import { FullWidthComponent } from './layout/full-width/full-width.component';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DefaultComponent,
+    FullWidthComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
